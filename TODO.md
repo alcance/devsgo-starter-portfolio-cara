a6b2d3e0b86e993fd1f38012c06ac082eece2876
Ivan Martinez <ivanmxdev@gmail.com>
2020-03-25 02:01:28 -0700

    :pencil: renamed notes to todo

---
+++ TODO.md
@@ -0,0 +1,12 @@
+# Todo
+
+- Auth0 integration
+- Auth0 Service
+- Database Modeling
+- Investigate Gatsby Backend integration
+
+# Notes
+
+https://auth0.com/blog/securing-gatsby-with-auth0/
+https://www.gatsbyjs.org/blog/2019-03-21-add-auth0-to-gatsby-livestream/#authentication-in-gatsby
+https://github.com/jlengstorf/gatsby-auth0-app/blob/master/.env.EXAMPLE
--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,5 +1,17 @@
 import React from "react";
+import { AuthService, useAuth } from "gatsby-theme-auth0";
 
-const Login = () => <h1>Login</h1>;
+export default () => {
+  const { isLoggedIn, profile } = useAuth();
 
-export default Login;
+  return (
+    <div>
+      {profile && <p>Hello {profile.name}</p>}
+      {isLoggedIn ? (
+        <button onClick={AuthService.logout}>Logout</button>
+      ) : (
+        <button onClick={AuthService.login}>Login</button>
+      )}
+    </div>
+  );
+};
